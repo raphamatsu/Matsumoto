d682cfea4d2e25a9bcee8e7b0ea7e3474865f965
raphamatsu <32023950+raphamatsu@users.noreply.github.com>
2017-09-16 18:49:09 -0300

    Primeiro teste

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+var aluno = {
+nome:"Raphael",
+telefone: "48 3028-1077",
+idade: "30"
+}
+
+function mudarcor(){
+var titulo = document.querySelector("h1");
+titulo.style.color = "red";	
+titulo.innerHTML = "SPFC";	
+}
